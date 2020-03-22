233b56e8cf994c3965ff0c42d14a7ce126b26ac5
victoriaharutyunyan <viktorya_harutyunyan@edu.aua.am>
2020-03-22 22:25:05 +0400

    json file was created and data used for operations was being taken from json file. Quiz questions were asked and the time was calculated for each question.

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 from threading import Timer
+import json
+with open("scratch_1.json") as f:
+    data = json.load(f)
 
-print ("Question1: What is the longest river in South America")
-correct = "Uruguay"
 score = 0
 questions_asked = 0
 count = 0
@@ -15,60 +16,31 @@ def hello():
   if inProgress:
    Timer(1.0, hello).start()
 
-hello()
 
-options = input ({'option1': "Uruguay",
-  'option2': "Orinoco",
-  'option3': "Amazon",
-  'option4':"Cauca",
-})
+for obj in data['Questions']:
+    count = 0
+    inProgress = True
+    hello()
+    temp = obj['correct']
+    del obj['correct']
+    options = input (obj)
+    if options == temp:
+        print ("True")
+        inProgress = False
+        score = score + 1
+        questions_asked = questions_asked + 1
+        thisCount = str(count)
+        print("you have spent " + thisCount + " seconds on this task")
+    else:
+     print ("False")
+     inProgress = False
+     thisCount = str(count)
+     print("you have spent " + thisCount + " seconds on this task")
+     questions_asked = questions_asked + 1
 
-while options == correct:
- print ("True")
- inProgress = False
- score = score + 1
- questions_asked = questions_asked + 1
- thisCount = str(count)
- print("you have spent " + thisCount + " seconds on this task")
- break
+if score >= 0:
+ print ("you finished the test and this is your score", score)
 else:
-  print ("False")
-  inProgress = False
-  thisCount = str(count)
-  print("you have spent " + thisCount + " seconds on this task")
-  questions_asked = questions_asked + 1
+ print ("you cannot get any score,you lose")
 
-count = 0
-inProgress = True
-print("What is the largest lake in Africa?")
-correct2 = "Victoria"
-hello()
-options2 = input ({'a': "Tanganika",
-'b':"Karita",
-'c':"Tana",
-'d':"Victoria"})
-while options2 == correct2:
-   inProgress = False
-   thisCount = str(count)
-   print("you have spent " + thisCount + " seconds on this task")
-   score = score + 1
-   questions_asked = questions_asked + 1
-   break
-else:
-   print("False")
-   inProgress = False
-   thisCount = str(count)
-   print("you have spent " + thisCount + " seconds on this task")
-   score = score - 1
-   questions_asked = questions_asked + 1
 
-while questions_asked == 2:
-   if score >= 0:
-    print ("you finished the test and this is your score", score)
-    break
-   else:
-     print ("you cannot get any score,you lose")
-     break
-
-else:
-  questions_asked = questions_asked + 1
